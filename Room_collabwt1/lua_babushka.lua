070db343ae5d7b2b0d917d3b8d1a574e06d78fc6
Kelli (Brush$troke) <166767965+Brushstroke90@users.noreply.github.com>
2024-08-06 18:24:20 -0400

    update hub/compression
    updated hub model. Added volume test at intro. Ran compression(hadnt been done yet)

--- Room_collabwt1/lua_babushka.lua
+++
@@ -1,5 +0,0 @@
-if callType == LuaCallType.Init then
-    for index, model in pairs(matryoshka) do
-        model.transform.GetChild(1).transform.parent = matryoshkaLid[index].transform
-    end
-end
--- Room_collabwt1/lua_passport.lua
+++
@@ -1,3 +0,0 @@
-if callType == LuaCallType.Init then
-    passport.lightStrengthMultiplier = 0.01
-end
